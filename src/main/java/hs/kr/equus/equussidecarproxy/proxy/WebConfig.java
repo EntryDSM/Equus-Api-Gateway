278e43bdf096c79eea4fce3a752d244ce7284c0f
HyunSu1768 <azxcv1768@gmail.com>
2024-05-09 15:52:45 +0900

    feat :: CORS 추가

---
+++ src/main/java/hs/kr/equus/equussidecarproxy/proxy/WebConfig.java
@@ -0,0 +1,19 @@
+package hs.kr.equus.equussidecarproxy.proxy;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:3001",
+                        "http://localhost:3002"
+                );
+    }
+}
