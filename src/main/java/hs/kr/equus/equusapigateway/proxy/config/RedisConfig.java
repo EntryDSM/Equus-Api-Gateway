51f1d737ced2a55a8d1d39cc92f986e63b45f11a
HyunSu1768 <azxcv1768@gmail.com>
2024-05-19 20:36:26 +0900

    redisTemplate 사용

---
+++ src/main/java/hs/kr/equus/equusapigateway/proxy/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package hs.kr.equus.equusapigateway.proxy.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
--- src/main/java/hs/kr/equus/equusapigateway/proxy/filter/CustomGlobalAuthFilter.java
+++ src/main/java/hs/kr/equus/equusapigateway/proxy/filter/CustomGlobalAuthFilter.java
@@ -1,15 +1,10 @@
 package hs.kr.equus.equusapigateway.proxy.filter;
 
-import io.lettuce.core.KeyValue;
-import io.lettuce.core.RedisClient;
-import io.lettuce.core.api.reactive.RedisReactiveCommands;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -18,14 +13,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @Component
 @Slf4j
 public class CustomGlobalAuthFilter extends AbstractGatewayFilterFactory<CustomGlobalAuthFilter.Config> {
-    private final RedisReactiveCommands<String, String> redisReactiveCommands;
+    private final RedisTemplate<String, String> redisTemplate;
 
-    public CustomGlobalAuthFilter(RedisReactiveCommands<String, String> redisReactiveCommands) {
+    public CustomGlobalAuthFilter(RedisTemplate<String, String> redisTemplate) {
         super(Config.class);
-        this.redisReactiveCommands = redisReactiveCommands;
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
@@ -48,32 +45,26 @@ public class CustomGlobalAuthFilter extends AbstractGatewayFilterFactory<CustomG
             String token = authorizationHeader.replaceFirst("^Bearer ", "");
             String key = "hs.kr.equus.user.domain.user.domain.UserInfo:" + token;
 
-            return redisReactiveCommands.hgetall(key)
-                    .collectMap(KeyValue::getKey, KeyValue::getValue)
-                    .flatMap(userInfoMap -> {
-                        log.debug("userInfo :: {}", userInfoMap);
+            Map<Object, Object> userInfoMap = redisTemplate.opsForHash().entries(key);
 
-                        if (userInfoMap.isEmpty()) {
-                            return mutateRequestWithForbiddenHeaders(exchange, chain);
-                        } else {
-                            String userId = userInfoMap.getOrDefault("userId", "FORBIDDEN");
-                            String userRole = userInfoMap.getOrDefault("userRole", "USER");
+            log.debug("userInfo :: {}", userInfoMap);
 
-                            log.debug("Extracted userId: {}", userId);
-                            log.debug("Extracted userRole: {}", userRole);
+            if (userInfoMap.isEmpty()) {
+                return mutateRequestWithForbiddenHeaders(exchange, chain);
+            } else {
+                String userId = (String) userInfoMap.getOrDefault("userId", "FORBIDDEN");
+                String userRole = (String) userInfoMap.getOrDefault("userRole", "USER");
 
-                            ServerHttpRequest modifiedRequest = request.mutate()
-                                    .header("Request-User-Id", userId)
-                                    .header("Request-User-Role", userRole)
-                                    .build();
+                log.debug("Extracted userId: {}", userId);
+                log.debug("Extracted userRole: {}", userRole);
 
-                            return chain.filter(exchange.mutate().request(modifiedRequest).build());
-                        }
-                    })
-                    .onErrorResume(e -> {
-                        log.error("Error during Redis access", e);
-                        return buildErrorResponse(exchange, HttpStatus.INTERNAL_SERVER_ERROR);
-                    });
+                ServerHttpRequest modifiedRequest = request.mutate()
+                        .header("Request-User-Id", userId)
+                        .header("Request-User-Role", userRole)
+                        .build();
+
+                return chain.filter(exchange.mutate().request(modifiedRequest).build());
+            }
         };
     }
 
@@ -96,25 +87,5 @@ public class CustomGlobalAuthFilter extends AbstractGatewayFilterFactory<CustomG
     }
 
     public static class Config {
-    }
-}
-
-@Configuration
-class RedisConfig {
-
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-    @Bean
-    public RedisClient redisClient() {
-        return RedisClient.create("redis://" + redisHost + ":" + redisPort);
-    }
-
-    @Bean
-    public RedisReactiveCommands<String, String> redisReactiveCommands(RedisClient redisClient) {
-        return redisClient.connect().reactive();
     }
 }
